b3cccca707ad4cac1ce4d8d28f34d87f3edc4543
Shannon Roberts <smrobertsllc@gmail.com>
2020-01-23 22:34:20 -0500

    resetting for deploy

---
+++ app/data/friends.js
@@ -0,0 +1,63 @@
+var friends = [
+    {
+        name: "Prince Florian",
+        photo:
+            "https://vignette.wikia.nocookie.net/disney/images/4/43/Profile_-_The_Prince.png/revision/latest?cb=20190312150050",
+        scores: [3, 4, 5, 5, 4, 1, 5, 1, 5, 3]
+    },
+    {
+        name: "Prince Charming",
+        photo:
+            "https://vignette.wikia.nocookie.net/disney/images/e/ec/Profile_-_Prince_Charming.png/revision/latest?cb=20190312150814",
+        scores: [4, 3, 5, 5, 3, 1, 5, 1, 5, 5]
+    },
+    {
+        name: "Prince Philip",
+        photo:
+            "https://vignette.wikia.nocookie.net/disney/images/8/86/Profile_-_Prince_Phillip.png/revision/latest?cb=20190312121844",
+        scores: [3, 3, 5, 5, 2, 2, 5, 1, 5, 3]
+    },
+    {
+        name: "Prince Eric",
+        photo:
+            "https://vignette.wikia.nocookie.net/disney/images/f/f1/Profile_-_Prince_Eric.jpeg/revision/latest?cb=20190312045000",
+        scores: [4, 5, 5, 5, 4, 3, 5, 2, 5, 5]
+    },
+    {
+        name: "Prince Adam",
+        photo:
+            "https://i.pinimg.com/originals/cf/22/ae/cf22ae53058a8780ed0878c034de56f5.jpg",
+        scores: [2, 4, 3, 5, 1, 1, 5, 3, 5, 2]
+    },
+    {
+        name: "Aladdin",
+        photo:
+            "https://vignette.wikia.nocookie.net/disney/images/b/bb/Profile_-_Aladdin.png/revision/latest?cb=20190829033153",
+        scores: [5, 4, 5, 1, 5, 4, 1, 5, 1, 5]
+    },
+    {
+        name: "John Smith",
+        photo:
+            "https://foxhugh.files.wordpress.com/2016/10/pocahontas-john-smith.jpg?w=452",
+        scores: [5, 3, 3, 1, 1, 3, 4, 3, 1, 3]
+    },
+    {
+        name: "Prince Naveen",
+        photo:
+            "https://bookprincessreviews.files.wordpress.com/2018/02/naveen.jpg?w=584",
+        scores: [3, 3, 3, 1, 5, 3, 3, 4, 1, 3]
+    },
+    {
+        name: "Eugene",
+        photo:
+            "https://i.pinimg.com/originals/7f/1d/64/7f1d641d8ace36e2d3a9ac89c7f33e92.jpg",
+        scores: [5, 2, 3, 1, 1, 5, 2, 5, 1, 2]
+    },
+    {
+        name: "Kristoff",
+        photo:
+            "https://imgix.bustle.com/rehost/2016/9/13/0844ddcd-5ba9-4acf-b1bd-1373a1025c75.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70",
+        scores: [4, 5, 4, 1, 1, 5, 4, 4, 1, 2]
+    }
+];
+module.exports = friends;
